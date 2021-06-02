8bc34d7eeccf5e4c830a35d33741c556c522c748
MEDHA <41751303+MEDHA-TIWARI@users.noreply.github.com>
2021-06-02 17:17:41 +0530

    Add files via upload
    We can pass functions as parameters to other functions and call them inside the outer functions. So callback is a function that is passed to another function. When the first function is done, it will run the second function.

--- Day3/callback.js
+++ Day3/callback.js
@@ -1,1 +1,13 @@
+// function
+function greet(name, callback){
+    console.log('Heyaa' + ' ' + 'name');
+    callback();
+}
 
+// callback function
+function callMe() {
+    console.log("This i call back function");
+}
+
+// passing the function as parameter
+greet('MEDHA', callMe);
