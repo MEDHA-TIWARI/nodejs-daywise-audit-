9b0ee88a340e2333d1ee365709cb1d7a62ebcd21
MEDHA <41751303+MEDHA-TIWARI@users.noreply.github.com>
2021-06-02 15:31:49 +0530

    Add files via upload

---
+++ Day 2/calculator.js
@@ -0,0 +1,19 @@
+// let's us make a basic calc to check all the arithmetic functions
+var x = 25;
+var y = 10;
+var add;
+var sub;
+var div, mul;
+add = x+y;
+sub = x-y;
+div = x/y;
+mul = x*y;
+
+//addition
+console.log(" Addition of x and y : ", add);
+//subtract
+console.log(" Subtraction of y from x : ", sub);
+//division
+console.log(" Division of x and y : ", div);
+//multiplication
+console.log(" Multiplication of x and y : ", mul);
