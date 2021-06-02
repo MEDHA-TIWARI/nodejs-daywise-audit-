8e6c2ff9edd147a64a494754f059695689d5c487
MEDHA <41751303+MEDHA-TIWARI@users.noreply.github.com>
2021-06-02 15:31:30 +0530

    Add files via upload

--- Day 2/demo1.js
+++ Day 2/demo1.js
@@ -1,1 +1,17 @@
+var x = 25;
+var y = 25;
 
+//Let's try to print the values
+
+console.log(" Value of X is " +x);
+console.log(" Value of Y is " +y);
+
+//Let's compare the the variables
+
+if(x==y)
+{
+    console.log(" Value of X is equal to value of Y ");
+}else
+{
+    console.log(" X and Y are not equal ");
+}
