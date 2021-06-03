71b522ae16ff9370e1597b921ad40619f5e790fd
MEDHA <41751303+MEDHA-TIWARI@users.noreply.github.com>
2021-06-03 21:12:02 +0530

    Add files via upload

---
+++ Day 5/expintern/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+module.exports = router;
