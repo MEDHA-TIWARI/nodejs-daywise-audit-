7043810fc73d7763848386ea2eaf490d0492afe6
MEDHA <41751303+MEDHA-TIWARI@users.noreply.github.com>
2021-06-03 23:50:00 +0530

    Add files via upload

---
+++ Day 6/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+router.get('/master', function(req, res, next) {
+  res.render('master');
+});
+
+module.exports = router;
